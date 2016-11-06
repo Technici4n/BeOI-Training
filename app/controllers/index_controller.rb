470bcc8ffd80bc3c8def9e96fe565340a81050b5
Technici4n <bruno.ploumhans@gmail.com>
2016-11-06 15:22:19 +0100

    Filter users for the tracker

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,6 +3,12 @@ require "uri"
 
 class IndexController < ApplicationController
 	def index
+		if params[:users]
+			user_list = User.where(id: params[:users])
+		else
+			user_list = User.all
+		end
+		@users = user_list.map{ |u| [u.id, ["", u.uva, u.display_name, u.id == session[:user_id], u.is_contestant]] }.to_h
 	end
 	
 	def tracker
