a53158f2696ec46ac43147830fb7707951d3f77c
Technici4n <bruno.ploumhans@gmail.com>
2016-11-06 15:30:52 +0100

    Shouldn't remove used stuff

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,2 +1,13 @@
 module IndexHelper
+	def self.users_to_hash(session) # Problemsets
+		format = {}
+		User.all.each do |u|
+			format[u.id] = ["", u.uva, u.display_name, false, u.is_contestant]
+		end
+		if User.exists?(id: session[:user_id])
+			u = User.find(session[:user_id])
+			format[u.id][3] = true
+		end
+		return format
+	end
 end
