97d7495527af56293d7497254d733d05b0456db3
Technici4n <bruno.ploumhans@gmail.com>
2016-11-26 13:27:56 +0100

    Added [ci] tag (shortcut for [code=inline])

--- app/assets/javascripts/index/uva-tracker.js
+++ app/assets/javascripts/index/uva-tracker.js
@@ -15,7 +15,7 @@ var UvaTracker = (function()
 	var uhunt_user_submissions_url = uhunt_domain + "subs-user/{0}"; // Param: user ID
 	var uhunt_all_problems_url = uhunt_domain + "p";
 	
-	var uva_problem_url = "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem={0}?category=";
+	var uva_problem_url = "https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem={0}&category=";
 
 	// HTML tags IDs
 	var html_id_last_submissions_table = "#lastsubmissions";
