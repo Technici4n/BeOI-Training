8a57e65c25297a955dc240b2c9c7d603681a3600
Technici4n <bruno.ploumhans@gmail.com>
2017-05-31 21:55:51 +0200

    Fix ordering bug

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,9 +1,9 @@
 class Subject < ApplicationRecord
 	validates :title, presence: true, length: {maximum: 60}
-	
-	has_many :forum_messages, dependent: :destroy
+
+	has_many :forum_messages, -> { order(created_at: :desc) }, dependent: :destroy
 	has_many :following_subjects, dependent: :destroy
 	has_many :following_users, through: :following_subjects, source: :user
-	
+
 	self.per_page = 10
 end
